98dd742022dd36054524933e509155f6590b2b0c
Minh Pham <mpham@naver.com>
2019-04-27 16:53:54 +0200

    Make connection to MongoDB

--- server.js
+++ server.js
@@ -10,12 +10,23 @@ const bodyParser = require("body-parser");
 // require cors
 const cors = require("cors");
 
+// require mongoose
+const mongoose = require("mongoose");
+
 // define local port
 const PORT = 5000;
 
 // add cors and bodyParser.json
 app.use(cors());
 app.use(bodyParser.json());
+
+// make connection to MongoDB (mongo URI, config obj)
+mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
+// retrieve connection
+const connection = mongoose.connection;
+connection.once("open", function() {
+  console.log("MongoDB connection established successfully");
+});
 
 // start up the server on defined port
 app.listen(PORT, function() {
